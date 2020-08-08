530c1b19e11a8733e177a61bdb6ca861118241d7
yashiki25 <ymk.gr.22@gmail.com>
2020-08-09 03:04:51 +0900

    クラスのオートロードを実装

---
+++ app/core/ClassLoader.php
@@ -0,0 +1,39 @@
+<?php
+
+class ClassLoader
+{
+    protected $dirs;
+
+    /**
+     * オートロードクラスを登録する
+     */
+    public function register(): void
+    {
+        spl_autoload_register([$this, 'loadClass']);
+    }
+
+    /**
+     * ディレクトリを登録する
+     * @param string $dir
+     */
+    public function registerDir(string $dir): void
+    {
+        $this->dirs[] = $dir;
+    }
+
+    /**
+     * クラスファイルを読み込む
+     * @param string $class
+     */
+    public function loadClass(string $class): void
+    {
+        foreach ($this->dirs as $dir) {
+            $file = "{$dir}/{$class}.php";
+            if (is_readable($file)) {
+                require $file;
+
+                return;
+            }
+        }
+    }
+}
