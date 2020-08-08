52122859ac3cc47c17960f28329fb76815278cd7
yashiki25 <ymk.gr.22@gmail.com>
2020-08-09 04:51:08 +0900

    コメント追加

--- app/core/Response.php
+++ app/core/Response.php
@@ -8,6 +8,9 @@ class Response
     private $statusText = 'OK';
     private $httpHeaders = [];
 
+    /**
+     * レスポンス送信
+     */
     public function send(): void
     {
         header("HTTP/1.1{$this->statusCode} {$this->statusText}");
@@ -16,21 +19,36 @@ class Response
             header("{$name}: {$value}");
         }
 
+        // 送信
         echo $this->content;
     }
 
+    /**
+     * HTMLなどクライアントに返すコンテンツをセット
+     * @param $content
+     */
     public function setContent($content): void
     {
         $this->content = $content;
     }
 
+    /**
+     * ステータスコードをセット
+     * @param int $statusCode
+     * @param string $statusText
+     */
     public function setStatusCode(int $statusCode, string $statusText): void
     {
         $this->statusCode = $statusCode;
         $this->statusText = $statusText;
     }
 
-    public function setHttpHeader($name, $value)
+    /**
+     * HTTPヘッダをセット
+     * @param $name
+     * @param $value
+     */
+    public function setHttpHeader($name, $value): void
     {
         $this->httpHeaders[$name] = $value;
     }
