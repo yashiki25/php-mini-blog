3ea0eb353f793a4688ede3a8a8d87e18f8f6c666
yashiki25 <ymk.gr.22@gmail.com>
2020-08-09 16:56:11 +0900

    ビューを実装

---
+++ app/core/View.php
@@ -0,0 +1,48 @@
+<?php
+
+class View
+{
+    private string $baseDir;
+    private array $defaults;
+    private array $layoutVariable = [];
+
+    public function __construct(string $baseDir, array $defaults = [])
+    {
+        $this->baseDir = $baseDir;
+        $this->defaults = $defaults;
+    }
+
+    public function setLayoutVar(string $name, string $value): void
+    {
+        $this->layoutVariable[$name] = $value;
+    }
+
+    public function render(string $_path, array $_variables = [], bool $_layout = false): string
+    {
+        $_file = "{$this->baseDir}/{$_path}.php";
+
+        extract(array_merge($this->defaults, $_variables));
+
+        ob_start();
+        ob_implicit_flush(0);
+
+        require $_file;
+
+        $content = ob_get_clean();
+
+        if ($_layout) {
+            $content = $this->render($_layout,
+                array_merge($this->layoutVariable, [
+                    '_content' => $content,
+                ])
+            );
+        }
+
+        return $content;
+    }
+
+    public function escape(string $string): string
+    {
+        return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
+    }
+}
