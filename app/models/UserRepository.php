59f495333aa6b7c5be0e2c5e0bcd733693c87114
yashiki25 <ymk.gr.22@gmail.com>
2020-08-23 01:47:35 +0900

    Add Userデータを扱うリポジトリを作成

---
+++ app/models/UserRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+class UserRepository extends DBRepository
+{
+    public function insert(string $userName, string $password)
+    {
+        $password = $this->hashPassword($password);
+        $now = new DateTime();
+
+        $sql = "
+            INSERT INTO user(user_name, password, created_at)
+            VALUE(:user_name, :password, :created_at)
+            ";
+
+        $stmt = $this->execute($sql, [
+            ':user_name'  => $userName,
+            ':password'   => $password,
+            ':created_at' => $now
+        ]);
+    }
+
+    public function hashPassword(string $password): string
+    {
+        return sha1($password . 'SecretKey');
+    }
+
+    public function fetchByUserName(string $userName)
+    {
+        $sql = "SELECT * FROM user WHERE user_name = :user_name";
+
+        return $this->fetch($sql, [
+            ':user_name' => $userName
+        ]);
+    }
+
+    /**
+     * ユーザーIDがユニークか
+     * @param string $userName
+     * @return bool
+     */
+    public function isUniqueUserName(string $userName): bool
+    {
+        $sql = "SELECT * FROM user WHERE user_name = :user_name";
+
+        $row = $this->fetch($sql, [
+            ':user_name' => $userName
+        ]);
+
+        if ($row['count'] === 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
