7341668de159af7dc7ee478957b61747eff63730
yashiki25 <ymk.gr.22@gmail.com>
2020-08-23 00:23:08 +0900

    不要なスペースを削除

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,6 @@
 require 'core/ClassLoader.php';
 
 $loader = new ClassLoader();
-$loader->registerDir(__DIR__.' /core');
-$loader->registerDir(__DIR__.' /models');
+$loader->registerDir(__DIR__.'/core');
+$loader->registerDir(__DIR__.'/models');
 $loader->register();
