d09df08be4ae2947920a166b6b0034d707bbeaf9
yashiki25 <ymk.gr.22@gmail.com>
2020-09-06 05:34:52 +0900

    Add ログイン・ログアウト処理を作成

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -4,6 +4,10 @@ class AccountController extends Controller
 {
     public function signupAction()
     {
+        if ($this->session->isAuthenticated()) {
+            return $this->redirect('/account');
+        }
+
         return $this->render([
             'user_name' => '',
             'password'  => '',
@@ -11,8 +15,25 @@ class AccountController extends Controller
         ]);
     }
 
+    public function signinAction()
+    {
+        if ($this->session->isAuthenticated()) {
+            return $this->redirect('/account');
+        }
+
+        return $this->render([
+            'user_name' => '',
+            'password'  => '',
+            '_token'    => $this->generateCsrfToken('account/signin')
+        ]);
+    }
+
     public function registerAction()
     {
+        if ($this->session->isAuthenticated()) {
+            return $this->redirect('/account');
+        }
+
         if (!$this->request->isPost()) {
             $this->forward404();
         }
@@ -57,5 +78,70 @@ class AccountController extends Controller
             'errors'    => $errors,
             '_token'    => $this->generateCsrfToken('account/signup'),
         ], 'signup');
+    }
+
+    public function authenticateAction()
+    {
+        if ($this->session->isAuthenticated()) {
+            return $this->redirect('/account');
+        }
+
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if (!$this->checkCsrfToken('account/signin', $token)) {
+            return $this->redirect('/account/signin');
+        }
+
+        $userName = $this->request->getPost('user_name');
+        $password = $this->request->getPost('password');
+
+        $errors = [];
+
+        if (!mb_strlen($userName)) {
+            $errors[] = 'ユーザIDを入力してください';
+        }
+
+        if (!mb_strlen($password)) {
+            $errors[] = 'パスワードを入力してください';
+        }
+
+        if (count($errors) === 0) {
+            $userRepository = $this->dbManager->get('User');
+            $user = $userRepository->fetchByUserName($userName);
+
+            if (!$user || ($user['password'] !== $userRepository->hashPassword($password))) {
+                $errors[] = 'ユーザIDかパスワードが不正です';
+            } else {
+                $this->session->setAuthenticated(true);
+                $this->session->set('user', $user);
+
+                return $this->redirect('/');
+            }
+        }
+
+        return $this->render([
+            'user_name' => $userName,
+            'password'  => $password,
+            'errors'    => $errors,
+            '_token'    => $this->generateCsrfToken('account/signin')
+        ], 'signin');
+    }
+
+    public function signoutAction()
+    {
+        $this->session->clear();
+        $this->session->setAuthenticated(false);
+
+        return $this->redirect('/account/signin');
+    }
+
+    public function indexAction()
+    {
+        $user = $this->session->get('user');
+
+        return $this->render(['user' => $user]);
     }
 }
--- app/views/layout.php
+++ app/views/layout.php
@@ -15,6 +15,18 @@
         </h1>
     </div>
 
+    <div id="nav">
+        <p>
+            <?php if ($session->isAuthenticated()): ?>
+            <a href="<?php echo $base_url; ?>/">ホーム</a>
+            <a href="<?php echo $base_url; ?>/account">アカウント</a>
+            <?php else: ?>
+            <a href="<?php echo $base_url; ?>/account/signin">ログイン</a>
+            <a href="<?php echo $base_url; ?>/account/signup">アカウント登録</a>
+            <?php endif; ?>
+        </p>
+    </div>
+
     <div>
         <?php echo $_content; ?>
     </div>
